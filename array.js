167af95589e3fe29a0682a94b53922a8de4b85f5
Manvendra Pratap Singh <106961889+Man7398@users.noreply.github.com>
2023-03-30 23:05:53 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,8 @@
+const arr=['toshi','tjsdo','jackal','jackuy']
+const ages=[29,32,31,13]
+console.log(arr);
+
+//module.exports ='hello';
+module.exports ={
+    arr,ages
+};
---
+++ modules.js
@@ -0,0 +1,8 @@
+const { arr,ages}=require('./array')
+//console.log(xuz);
+// [ 'toshi', 'tjsdo', 'jackal', 'jackuy' ]
+// {} - this value is empty 
+console.log(arr,ages);
+const os=require('os');
+console.log(os);
+console.log(os.platform(), os.homedir());
