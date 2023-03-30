a4e73d767332a413c653cee5481c2f23da56dbc4
Manvendra Pratap Singh <106961889+Man7398@users.noreply.github.com>
2023-03-30 22:41:37 +0530

    Add files via upload

---
+++ global.js
@@ -0,0 +1,20 @@
+//Global Object
+console.log(global);
+
+// global.setTimeout(() => {
+//    console.log('in the momment') 
+// }, 3000);
+
+// setTimeout(() => {
+//     console.log('in the momment') 
+//      }, 3000);
+
+//SetInterval -It constantly pings the object
+//  const int=setInterval(()=>
+    //  {
+    //     console.log('ellp');
+    //  },1000)
+     
+    //  console.log(__dirname); 
+    //  console.log(__filename); 
+     console.log(document.querySelector); //document is not defined
